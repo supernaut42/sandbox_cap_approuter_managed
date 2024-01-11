07e3d97e2d4c36af7c9c8660fa9ae69f4abe3982
Peter Gilberg <peter.gilberg@orbis.de>
2024-01-11 13:11:58 +0000

    Added support for managed approuter

--- srv/dummy-service.js
+++ srv/dummy-service.js
@@ -7,12 +7,12 @@ class DummyService extends cds.ApplicationService {
 
       if (!req.user.is('SandboxMember')) {
 
-        return [{ someNumer: 99, someText: "You're not a member." }];
+        return [{ someNumer: 99, someText: `Sorry ${req.user.id}. You're not a member.` }];
 
       } else {
 
         return [
-
+          { someNumber: 1, someText: `Welcome ${req.user.id}. These are the rules of the Fight Club.` },
           { someNumber: 1, someText: "The first rule of Fight Club is: you do not talk about Fight Club" },
           { someNumber: 2, someText: "The second rule of Fight Club is: you DO NOT talk about Fight Club! " },
           { someNumber: 2, someText: "Third rule of Fight Club: someone yells 'stop!', goes limp, taps out, the fight is over." }]
