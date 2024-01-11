e0dfb8809d9c74bbfde37369f5cd5e1c124e2de9
Peter Gilberg <peter.gilberg@orbis.de>
2024-01-11 11:03:28 +0000

    Config for Standalone Approuter

--- srv/dummy-service.js
+++ srv/dummy-service.js
@@ -4,12 +4,20 @@ class DummyService extends cds.ApplicationService {
 
     this.on("READ", "DummyEntity", async (req) => {
 
-      return [
 
-        { someNumber: 1, someText: "The first rule of Fight Club is: you do not talk about Fight Club" },
-        { someNumber: 2, someText: "The second rule of Fight Club is: you DO NOT talk about Fight Club! " },
-        { someNumber: 2, someText: "Third rule of Fight Club: someone yells 'stop!', goes limp, taps out, the fight is over." }]
+      if (!req.user.is('SandboxMember')) {
 
+        return [{ someNumer: 99, someText: "You're not a member." }];
+
+      } else {
+
+        return [
+
+          { someNumber: 1, someText: "The first rule of Fight Club is: you do not talk about Fight Club" },
+          { someNumber: 2, someText: "The second rule of Fight Club is: you DO NOT talk about Fight Club! " },
+          { someNumber: 2, someText: "Third rule of Fight Club: someone yells 'stop!', goes limp, taps out, the fight is over." }]
+
+      }
     });
 
   }
